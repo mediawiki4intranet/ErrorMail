18a70a3e385f97cd6838dd3c9ce5a69d331b3be1
Vitaliy Filippov <vitalif@yourcmc.ru>
2012-07-09 14:41:35 +0000

    Bug 86009 - Handle fatal errors (without stack traces) and exceptions

--- ErrorMail.php
+++ ErrorMail.php
@@ -24,8 +24,9 @@ $wgExtensionCredits['other'][] = array(
 // Send error reports to these addresses:
 $wgErrorMail = array($wgEmergencyContact);
 
-// Report ALL error levels by default:
+// Report ALL error levels and exceptions by default:
 $wgErrorMailLevels = ~0;
+$wgErrorMailExceptions = true;
 
 // Also log reported errors into this file:
 $wgErrorMailLog = '';
@@ -52,28 +53,46 @@ class ErrorMail
         E_RECOVERABLE_ERROR => 'E_RECOVERABLE_ERROR',
         E_DEPRECATED        => 'E_DEPRECATED',
         E_USER_DEPRECATED   => 'E_USER_DEPRECATED',
+        'exception'         => 'Exception',
+    );
+
+    static $fatalErrors = array(
+        E_ERROR             => 'E_ERROR',
+        E_PARSE             => 'E_PARSE',
+        E_CORE_ERROR        => 'E_CORE_ERROR',
+        E_CORE_WARNING      => 'E_CORE_WARNING',
+        E_COMPILE_ERROR     => 'E_COMPILE_ERROR',
+        E_COMPILE_WARNING   => 'E_COMPILE_WARNING',
     );
 
     static function init()
     {
+        global $wgExceptionHooks;
         self::$oldHandler = set_error_handler(array(__CLASS__, 'handler'));
+        register_shutdown_function(array(__CLASS__, 'shutdownHandler'));
+        $wgExceptionHooks['MWException'][] = array(__CLASS__, 'mwExceptionHandler');
+        $wgExceptionHooks['MWExceptionRaw'][] = array(__CLASS__, 'mwExceptionHandler');
     }
 
-    static function handler($errno, $errstr, $errfile, $errline, $errcontext)
+    /**
+     * Main handler for errors ($errno = integer error level) and exceptions ($errno === 'exception')
+     */
+    static function handler($errno, $errstr, $errfile, $errline, $errcontext, $nostack = false)
     {
         global $wgUser, $wgServer, $wgSitename, $wgTitle, $IP;
         global $wgPasswordSender, $wgPasswordSenderName;
-        global $wgErrorMail, $wgErrorMailLog, $wgErrorMailLevels, $wgSuppressCount;
+        global $wgSuppressCount;
+        global $wgErrorMail, $wgErrorMailLog, $wgErrorMailLevels, $wgErrorMailExceptions;
         $enabled = $wgErrorMailLevels;
         $reporting = error_reporting();
         if ($reporting == 0 || isset($wgSuppressCount) && $wgSuppressCount > 0)
         {
-            // Handle error suppression operator (@)
+            // Handle PHP error suppression operator (@)
             // Also handle patched wfSuppressWarnings()
             $enabled = $reporting;
         }
         if (!self::$inProgress && ($wgErrorMail || $wgErrorMailLog) &&
-            ($enabled & $errno))
+            ($errno == 'exception' ? $wgErrorMailExceptions : $enabled & $errno))
         {
             self::$inProgress = true;
             if (!isset(self::$errorTypes[$errno]))
@@ -85,19 +104,29 @@ class ErrorMail
             $text = date("[Y-m-d H:i:s] ").$wgSitename.', ';
             $text .= 'Title: '.($wgTitle ? $wgTitle : '-')."\n";
             $text .= "User: $username, IP: ".wfGetIP()."\n\n";
-            $text .= "$errtype at $errfile:$errline\n".trim($errstr)."\n\n";
-            $text .= "Stack trace:\n";
-            $iplen = strlen($IP);
-            foreach (array_slice(debug_backtrace(0), 1) as $i => $frame)
+            if ($errno != 'exception')
             {
-                if (substr($frame['file'], 0, $iplen) == $IP)
-                    $frame['file'] = '.'.substr($frame['file'], $iplen);
-                $text .= "#$i: ";
-                if (isset($frame['class']))
-                    $text .= $frame['class'].$frame['type'];
-                $text .= "$frame[function] at $frame[file]:$frame[line]\n";
+                // Exception message already includes this information
+                $text .= "$errtype at $errfile:$errline\n";
             }
-            $text .= "\nURL: $wgServer".$_SERVER['REQUEST_URI']."\n";
+            $text .= trim($errstr)."\n\n";
+            if (!$nostack)
+            {
+                $text .= "Stack trace:\n";
+                $iplen = strlen($IP);
+                foreach (array_slice(debug_backtrace(0), 1) as $i => $frame)
+                {
+                    if (substr($frame['file'], 0, $iplen+1) == $IP.'/')
+                        $frame['file'] = './'.substr($frame['file'], $iplen+1);
+                    $text .= "#$i: ";
+                    if (isset($frame['class']))
+                        $text .= $frame['class'].$frame['type'];
+                    $text .= "$frame[function] at $frame[file]:$frame[line]\n";
+                }
+                $text .= "\n";
+            }
+            // Request details
+            $text .= "URL: $wgServer".$_SERVER['REQUEST_URI']."\n";
             $text .= "\n\$_GET = ".var_export($_GET, true).
                 "\n\$_POST = ".var_export($_POST, true).
                 "\n\$_COOKIE = ".var_export($_COOKIE, true)."\n";
@@ -127,5 +156,19 @@ class ErrorMail
             $args = func_get_args();
             call_user_func_array(self::$oldHandler, $args);
         }
+    }
+
+    static function shutdownHandler()
+    {
+        $error = error_get_last();
+        if ($error && isset(self::$fatalErrors[$error['type']]))
+            self::handler($error['type'], $error['message'], $error['file'], $error['line'], NULL);
+        return true;
+    }
+
+    static function mwExceptionHandler($error)
+    {
+        self::handler('exception', "$error", NULL, NULL, NULL, true);
+        return true;
     }
 }
